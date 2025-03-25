032839d510d06346af044cc55f6427362251c13a
ThatyssBoot <aflahv6bot@gmail.com>
2025-03-25 22:03:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -89,7 +89,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 # Links
 GRP_LNK = environ.get('GRP_LNK', 't.me/CINEMACOMPANYE')
-CHNL_LNK = environ.get('CHNL_LNK', 't.me/CINEMACOMPANYE')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+KjaThLecm9oyYWE1')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'CINEMACOMPANYE') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/iet_owner')
 
